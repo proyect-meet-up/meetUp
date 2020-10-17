763af680c8a106c213cbc53151cc5198e573842c
Borja Arana <barana1977@hotmail.com>
2020-10-17 20:17:55 +0200

    Implementado lógica mostrar o no buscador en distintas vistas

--- meet-up-client/src/app/app.component.ts
+++ meet-up-client/src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd, RouterEvent } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'meet-up';
+  title = "meet-up";
+  show: boolean = true;
+
+  constructor(private router: Router) {
+
+        this.router.events.subscribe((event: RouterEvent) => {
+          if (event instanceof NavigationEnd) {
+            if (
+              event.url.includes("detalle") ||
+              event.url.includes("login") ||
+              event.url.includes("register")
+            ) {
+              this.show = false;
+            } else {
+              this.show = true;
+            }
+          }
+        });
+  }
 }
--- meet-up-client/src/app/shared/buscador/buscador.component.ts
+++ meet-up-client/src/app/shared/buscador/buscador.component.ts
@@ -1,21 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-buscador',
   templateUrl: './buscador.component.html',
   styleUrls: ['./buscador.component.scss'],
 })
-export class BuscadorComponent {
-  public greaterThanValue = 0;
-  public lessThanValue = 1;
-  public isInvalid: boolean = false;
+export class BuscadorComponent implements OnInit{
 
-  onChange(event: any): void {
 
-    this.isInvalid = this.greaterThanValue > this.lessThanValue;
+
+  ngOnInit() {
+
   }
 
-  onSubmit(){
-    console.log(this.greaterThanValue > this.lessThanValue);
-  }
+
+
+
+
 }

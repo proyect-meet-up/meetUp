fc4135d4a93be8858ddf7819c7817c63ef30dc5a
Borja Arana <barana1977@hotmail.com>
2020-10-17 10:25:42 +0200

    Cambios merging eventos modulo

--- meet-up-client/src/app/eventos/eventos.module.ts
+++ meet-up-client/src/app/eventos/eventos.module.ts
@@ -9,16 +9,6 @@ import { ReservaEventoComponent } from './reserva-evento/reserva-evento.componen
 import { ListadoRegistradoComponent } from './listado-registrado/listado-registrado.component';
 import { DetalleRegistradoComponent } from './detalle-registrado/detalle-registrado.component';
 import { ListadoEventosComponent } from './listado-eventos/listado-eventos.component';
-<<<<<<< HEAD
-
-
-@NgModule({
-  declarations: [   
-    CardEventoComponent, 
-    DetalleEventoComponent, 
-    ReservaEventoComponent, 
-    ListadoRegistradoComponent, 
-=======
 
 
 
@@ -28,11 +18,10 @@ import { ListadoEventosComponent } from './listado-eventos/listado-eventos.compo
     DetalleEventoComponent,
     ReservaEventoComponent,
     ListadoRegistradoComponent,
->>>>>>> efe932b79e6c141eea74169f6c3fe9f627355308
     DetalleRegistradoComponent,
     ListadoEventosComponent
   ],
-  imports: [    
+  imports: [
     CommonModule,
     EventosRoutingModule
   ]

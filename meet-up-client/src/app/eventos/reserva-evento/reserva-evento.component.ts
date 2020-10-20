1032561fccf16a36ca4b26cb309bb085da651d3f
Borja Arana <barana1977@hotmail.com>
2020-10-20 19:05:05 +0200

    Creación formbuilder en componente reserva detalle

--- meet-up-client/src/app/eventos/reserva-evento/reserva-evento.component.ts
+++ meet-up-client/src/app/eventos/reserva-evento/reserva-evento.component.ts
@@ -2,32 +2,52 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Evento } from '../evento.model';
 import { EventoService } from '../evento.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Usuario } from 'src/app/usuario/usuario.model';
+import { ValidadoresService } from 'src/app/shared/services/validadores.service';
 
 @Component({
-  selector: 'app-reserva-evento',
-  templateUrl: './reserva-evento.component.html',
-  styleUrls: ['./reserva-evento.component.scss']
+  selector: "app-reserva-evento",
+  templateUrl: "./reserva-evento.component.html",
+  styleUrls: ["./reserva-evento.component.scss"],
 })
-
-
-export class ReservaEventoComponent implements OnInit{
-
+export class ReservaEventoComponent implements OnInit {
   evento: Evento;
   id: number;
-  person: any; // SIMULACIÓN CUANDO TENGAMSO EL OBJECTO USER
+  formularioReserva: FormGroup;
+  usuario: Usuario;
 
-
-  constructor( private route: ActivatedRoute, private eventosService: EventoService) {
-    this.id = +this.route.snapshot.params['id'];
+  constructor(
+    private route: ActivatedRoute,
+    private eventosService: EventoService,
+    private fb: FormBuilder,
+    private valdacionesService: ValidadoresService
+  ) {
+    this.id = +this.route.snapshot.params["id"];
     this.evento = this.eventosService.obtenerEvento(this.id);
-    this.person = {
-      nombre: 'Borja'
-    }
+    //this.usuario = new Usuario('Borja', 'Arana', 'borja@mail.com', '634584653')
+
   }
 
   ngOnInit(): void {
+    if( this.evento ) {
+      this.crearFormularioReserva();
+    }
   }
 
+  crearFormularioReserva() {
+    this.formularioReserva = this.fb.group({
+      nombre: [ this.usuario ? this.usuario.nombre : '', [Validators.required]],
+      apellido: [ this.usuario ? this.usuario.apellido : '', [Validators.required]],
+      email: [
+        this.usuario ? this.usuario.email: '',
+        [Validators.required, this.valdacionesService.comprobarValidacionEmail],
+      ],
+      telefono: [ this.usuario ? this.usuario.telefono: ''],
+    });
+  }
 
-
+  reservarEvento() {
+    console.log('Desde Formulario RESERVA', this.formularioReserva.value)
+  }
 }

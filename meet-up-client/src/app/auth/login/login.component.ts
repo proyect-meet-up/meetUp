f5e197e4b2b7dd482be812d1263a039ae2f65c1a
Gonza <tritio2@gmail.com>
2020-10-20 17:54:54 +0200

    Creadas validaciones en formulario login

--- meet-up-client/src/app/auth/login/login.component.ts
+++ meet-up-client/src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,13 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  formularioLogin: FormGroup;
+  hide: boolean = true;  
+  isvalidEmail = /\S+@\S+\.\S+/;
 
+  constructor(private fb: FormBuilder) { }
+
+  
   ngOnInit(): void {
+
+    this.formularioLogin = this.fb.group({
+      email: ['', [Validators.required, Validators.email, Validators.pattern(this.isvalidEmail)]],
+      password: ['', Validators.required]
+    }) 
+
   }
 
-  login() {
+  obtenerMensajeError(campo: string): string {
+    let mensaje;
+    if(this.formularioLogin.get(campo).errors?.required ) {
+      mensaje = 'Debes introducir un valor';
+    } else if (this.formularioLogin.get(campo).hasError('pattern')) {
+      mensaje = 'No es un email válido'
+    } else if (this.formularioLogin.get(campo).hasError('minlength')) {
+      const longitudMinima = this.formularioLogin.get(campo).errors?.minlength.requiredLength;
+      mensaje = `El campo debe tener un mínimo de ${longitudMinima} caracteres`
+    }
 
+    return mensaje;
+  }
+
+  NoEsCampoValido(campo: string) {
+    return (this.formularioLogin.get(campo).touched || this.formularioLogin.get(campo).dirty && 
+      !this.formularioLogin.get(campo).valid);
+  }
+  
+
+  login() {
+    if(this.formularioLogin.invalid) {
+      return;
+    }
+
+    console.log(this.formularioLogin.value);
   }
 
 }

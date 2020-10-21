1271987f6a1629a65d88b18960e50dadf6e86305
Borja Arana <barana1977@hotmail.com>
2020-10-21 10:49:39 +0200

    Introdución validaciones en el registro component.

--- meet-up-client/src/app/auth/register/register.component.ts
+++ meet-up-client/src/app/auth/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidadoresService } from 'src/app/shared/services/validadores.service';
 
 @Component({
   selector: "app-register",
@@ -11,25 +12,56 @@ export class RegisterComponent implements OnInit {
   hide2: boolean = true;
   formularioRegistro: FormGroup;
 
-  constructor(private fb: FormBuilder ) {}
+  constructor(
+    private fb: FormBuilder,
+    private validacionesService: ValidadoresService
+  ) {}
+
+  get passwordNoIguales() {
+    return (
+      this.formularioRegistro.get("confirmacionPassword").invalid &&
+      this.formularioRegistro.get("confirmacionPassword").touched
+    );
+  }
+
+  get emailUsuarioExiste() {
+    return (
+      this.formularioRegistro.get("email").invalid &&
+      this.formularioRegistro.get("email").touched
+    );
+  }
 
   ngOnInit(): void {
+    this.crearFormularioRegistro();
+  }
 
-    this.formularioRegistro = this.fb.group({
-      nombre: ['', Validators.required] ,
-      apellido: ['', Validators.required],
-      email: ['', Validators.email],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      confirmacionPassword: ['', Validators.required]
-    })
+  crearFormularioRegistro() {
+    this.formularioRegistro = this.fb.group(
+      {
+        nombre: ["", Validators.required],
+        apellido: ["", Validators.required],
+        email: [
+          "",
+          [this.validacionesService.comprobarValidacionEmail],
+          this.validacionesService.existeUsuarioEmail,
+        ],
+        password: ["", [Validators.required, Validators.minLength(6)]],
+        confirmacionPassword: ["", Validators.required],
+      },
+      {
+        validators: this.validacionesService.passwordIguales(
+          "password",
+          "confirmacionPassword"
+        ),
+      }
+    );
   }
 
   registro() {
-    if(this.formularioRegistro.invalid) {
-      return;
-    }
+    // if (this.formularioRegistro.invalid) {
+    //   return;
+    // }
 
-    console.log(this.formularioRegistro.value)
+    console.log(this.formularioRegistro.value);
   }
-  
 }

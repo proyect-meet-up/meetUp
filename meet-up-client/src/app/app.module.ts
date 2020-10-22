091bf0b24ac542fbdc84fc692c052fd09669c828
tritio <tritio2@gmail.com>
2020-10-22 19:11:41 +0200

    Creado componente para página no encontrada y modificado app-routing

--- meet-up-client/src/app/app.module.ts
+++ meet-up-client/src/app/app.module.ts
@@ -6,12 +6,13 @@ import { AppComponent } from './app.component';
 
 import { ComponentsModule } from './shared/components.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PageNotFoundComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,

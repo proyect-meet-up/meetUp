31a50e26210e255d88bd10ef8f4625f2d234de55
Borja Arana <barana1977@hotmail.com>
2020-11-19 11:22:41 +0100

    CLIENT: Refactorizando. Usuario componente  es el componente contralador.

--- client/src/app/privado/usuario/usuario/usuario.component.ts
+++ client/src/app/privado/usuario/usuario/usuario.component.ts
@@ -1,28 +1,25 @@
 
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit} from '@angular/core';
 import { Usuario } from '../usuario.model';
 
 @Component({
-  selector: "app-usuario",
-  templateUrl: "./usuario.component.html",
-  styleUrls: ["./usuario.component.scss"]
+  selector: 'app-usuario',
+  templateUrl: './usuario.component.html',
+  styleUrls: ['./usuario.component.scss'],
 })
 export class UsuarioComponent implements OnInit {
   usuario: Usuario;
 
-  formularioUsuario: FormGroup;
-  isvalidEmail = /\S+@\S+\.\S+/;
+  constructor(
 
-  hide: boolean = true;
-  hide2: boolean = true;
-
-  constructor(private fb: FormBuilder) { }
+  ) {}
 
   ngOnInit(): void {
-    this.usuario = new Usuario("Borja", "Arana", "barana@mail.com");
+    this.usuario = new Usuario('Borja', 'Arana', 'barana@mail.com');
   }
-  
- 
 
+  actualizarUsuario(event) {
+    console.log('Evento', event)
+
+  }
 }

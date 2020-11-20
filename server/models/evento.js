a03acf034e857f1e52d23aa8ae21ea722456f672
tritio <tritio2@gmail.com>
2020-11-20 19:03:21 +0100

    SERVER: Creado el modelo de evento

---
+++ server/models/evento.js
@@ -0,0 +1,48 @@
+
+const { Schema, model } = require('mongoose');
+
+
+const EventoSchema = Schema({
+    titulo: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    descripcion: {
+        type: String
+    },
+    direccion: {
+        required: true,
+        type: Schema.Types.ObjectId,
+        ref: 'Direccion'
+    },
+    categoria: {
+        required: true,
+        type: Schema.Types.ObjectId,
+        ref: 'Categoria'
+    },  
+    usuario: {
+        required: true,
+        type: Schema.Types.ObjectId,
+        ref: 'Usuario'
+    },
+    precio: {
+        type: Number
+    },
+    fecha: {
+        type: Date
+    }, 
+    tags: {
+        type: [ String ]
+    }
+    
+});
+
+// Opcional, para personalizar la data que se envía como respuesta en el json. Extraemos el password del objeto
+EventoSchema.method('toJSON', function() {
+    const { __v, ...object } = this.toObject();  
+    return object;
+})
+
+
+module.exports = model('Evento', EventoSchema);

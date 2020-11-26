c33b7dc041a7393f69dc37ea7941108f6fa39d1e
Borja Arana <barana1977@hotmail.com>
2020-11-26 19:31:36 +0100

    SERVER: Creado get Evento en el controllador

--- server/controllers/eventos.controller.js
+++ server/controllers/eventos.controller.js
@@ -5,65 +5,75 @@ const Evento = require('../models/evento');
 const Direccion = require('../models/direccion');
 
 const getEventos = async (req, res = response) => {
+	const eventos = await Evento.find()
+		.populate('usuario', 'nombre email')
+		.populate('categoria', 'categoria')
+		.populate('direccion', 'calle numero provincia codigo');
 
-    const eventos =  await Evento.find()
-                                .populate('usuario', 'nombre email')
-                                .populate('categoria', 'categoria')
-                                .populate('direccion', 'provincia codigo')
-                                
-    res.json ({
-        ok: true,
-        total: eventos.length,
-        eventos
-    });
+	res.json({
+		ok: true,
+		total: eventos.length,
+		eventos
+	});
+};
+
+const getEvento = async (req, res = response) => {
+	const evento = await Evento.findById(req.params.id)
+				.populate('direccion', 'calle numero localidad provincia codigo')
+
+	res.json({
+		ok: true,
+		evento
+	});
 };
 
 const crearEvento = async (req, res = response) => {
-    const uid = req.uid;
+	const uid = req.uid;
 
-    try {
-        // https://masteringjs.io/tutorials/mongoose/create
-        const nuevaDireccion = await Direccion.create(req.body.direccion);
+	try {
+		// https://masteringjs.io/tutorials/mongoose/create
+		const nuevaDireccion = await Direccion.create(req.body.direccion);
 
-        let { direccion, ...rest } = req.body;
+		let { direccion, ...rest } = req.body;
 
-        const evento = new Evento({
-            usuario: uid,
-            direccion: nuevaDireccion._id,
-            ...rest
-        });
+		const evento = new Evento({
+			usuario: uid,
+			direccion: nuevaDireccion._id,
+			...rest
+		});
 
-        const eventoDB = await evento.save();
+		const eventoDB = await evento.save();
 
-        res.status(201).json({
-            ok: true,
-            evento: eventoDB
-        });
-    } catch (error) {
-        res.status(500).json({
-            ok: false,
-            msg: 'Error de servidor, ni idea'
-        });
-    }
+		res.status(201).json({
+			ok: true,
+			evento: eventoDB
+		});
+	} catch (error) {
+		res.status(500).json({
+			ok: false,
+			msg: 'Error de servidor, ni idea'
+		});
+	}
 };
 
 const actualizarEvento = (req, res = response) => {
-    res.status(200).json({
-        ok: true,
-        msg: 'actualizarEvento'
-    });
+	res.status(200).json({
+		ok: true,
+		msg: 'actualizarEvento'
+	});
 };
 
 const borrarEvento = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'borrarEvento'
-    });
+	res.json({
+		ok: true,
+		msg: 'borrarEvento'
+	});
 };
 
 module.exports = {
-    getEventos,
-    crearEvento,
-    actualizarEvento,
-    borrarEvento
+	getEventos,
+	getEvento,
+	crearEvento,
+	actualizarEvento,
+	borrarEvento
 };

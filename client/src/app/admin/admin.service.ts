5e36e8b1f499be8b3e6f2a9519c671726ac4ed8e
Borja Arana <barana1977@hotmail.com>
2020-12-02 18:11:31 +0100

    CLIENT: Creada llamada actualizar eventos por el admin

--- client/src/app/admin/admin.service.ts
+++ client/src/app/admin/admin.service.ts
@@ -3,7 +3,7 @@ import { BehaviorSubject, Observable, Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Evento } from '../eventos/evento.model';
-import { pluck } from 'rxjs/operators';
+import { filter, pluck, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -32,11 +32,12 @@ export class AdminService {
     this.seleccionadoTodos.next(valor);
   }
 
-  obtenerTodosEventos(): Observable<Evento[]> {
+  obtenerTodosEventos(): Observable<any> {
     return this.http.get<Evento[]>(`${this.url}/eventos`)
-      .pipe(
-        pluck('eventos')
-      )
+      .pipe(pluck('eventos'))
+  }
 
+  actualizarEventosAConfirmar(eventos) {
+    return this.http.patch(`${this.url}/eventos`, eventos )
   }
 }

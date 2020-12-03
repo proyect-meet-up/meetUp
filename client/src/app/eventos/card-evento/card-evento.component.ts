9884b66830755257798e7741b893ece17ebd1b70
Borja Arana <barana1977@hotmail.com>
2020-12-03 18:04:17 +0100

    CLIENT:  Card evento redirecciona a detalle componente o al evento que hay que actualizar.

--- client/src/app/eventos/card-evento/card-evento.component.ts
+++ client/src/app/eventos/card-evento/card-evento.component.ts
@@ -1,21 +1,44 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnDestroy, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { Evento } from "../evento.model";
+import { AuthService } from '../../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
-  selector: "app-card-evento",
-  templateUrl: "./card-evento.component.html",
-  styleUrls: ["./card-evento.component.scss"],
+  selector: 'app-card-evento',
+  templateUrl: './card-evento.component.html',
+  styleUrls: ['./card-evento.component.scss'],
 })
-export class CardEventoComponent implements OnInit {
+export class CardEventoComponent implements OnInit, OnDestroy {
   @Input() curso: Evento;
   @Input() index: number;
+  @Input('url') url: string;
+  sub: Subscription;
+  tieneToken: boolean;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
   ngOnInit(): void {
-
+    this.sub = this.authService.estaLogueado$.subscribe(
+      (data) => (this.tieneToken = data)
+    );
   }
 
+  irDetalleEvento() {
+    if (this.url === 'eventosDelUsuario') {
+      this.router.navigate(['privado', 'eventos', 'usuario', this.curso._id]);
+    } else {
+      if (this.tieneToken) {
+        this.router.navigate(['privado', 'detalle-evento', this.curso._id]);
+      } else {
+        this.router.navigate(['detalle-evento', this.curso._id]);
+      }
+    }
+  }
 
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 }
+
+

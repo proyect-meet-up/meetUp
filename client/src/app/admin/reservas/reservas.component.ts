f1cff266ae98ac8a0aa72aeb98ed8f924160aabd
Borja Arana <barana1977@hotmail.com>
2020-12-03 17:57:47 +0100

    CLIENT:  Refactorización listado reservas. Se filtra sólo los eventos confirmados.

--- client/src/app/admin/reservas/reservas.component.ts
+++ client/src/app/admin/reservas/reservas.component.ts
@@ -1,33 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { diaDeHoy, formartearFecha } from '@shared/componentes/helpers/helpers';
 import { Evento } from 'src/app/eventos/evento.model';
 import { AdminService } from '../admin.service';
 
 @Component({
-  selector: "app-reservas",
-  templateUrl: "./reservas.component.html",
-  styleUrls: ["./reservas.component.scss"],
+  selector: 'app-reservas',
+  templateUrl: './reservas.component.html',
+  styleUrls: ['./reservas.component.scss'],
 })
 export class ReservasComponent implements OnInit {
-  displayedColumns: string[] = [
-    "nombre",
-    "titulo",
-    "fecha",
-    "direccion",
-    'detalle'
-  ];
   dataSource: Evento[] = [];
 
-  constructor( private adminService: AdminService ) {}
+  constructor(private adminService: AdminService) {}
 
   ngOnInit(): void {
     this.obtenerEventosReservados();
   }
 
   obtenerEventosReservados() {
-    this.adminService.obtenerTodosEventos()
-      .subscribe( (data: Evento[]) => {
-        let eventosConfirmados = data.filter( e => e.confirmar === true );
-        this.dataSource = [...eventosConfirmados];
-      })
+    this.adminService.obtenerTodosEventos().subscribe((data: Evento[]) => {
+      let eventosConfirmados = data.map(formartearFecha);
+      eventosConfirmados = eventosConfirmados
+        .filter((e) => e.fecha >= diaDeHoy())
+        .filter((e) => e.confirmar === true);
+      this.dataSource = [...eventosConfirmados];
+    });
   }
 }

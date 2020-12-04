20a9d0f3fdf8feb930b5285a918b5006c7a1b4a9
Borja Arana <barana1977@hotmail.com>
2020-12-04 13:00:00 +0100

    CLEINT: Implementación barra de navegación fija cuendo se empieza a hacer scroll hacia abajo.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,32 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, NavigationEnd, RouterEvent } from "@angular/router";
+import { Component, OnInit, Renderer2, ViewChild, ViewChildren, ElementRef, AfterViewInit, ContentChild, TemplateRef } from '@angular/core';
+import { Router, NavigationEnd, RouterEvent, Scroll } from "@angular/router";
 import { AuthService } from './auth/auth.service';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title = "Barum";
+  title = 'Barum';
   mostrarBuscador: boolean = true;
   estaLogueado: boolean = false;
   admin: boolean = false;
+  @ViewChild('headerscroll', { static: false }) headerScroll: ElementRef;
 
-  constructor(private router: Router, private authService: AuthService) {
-        this.router.events.subscribe((event: RouterEvent) => {
-          if (event instanceof NavigationEnd) {
-            if (
-              event.url.includes('login') ||
-              event.url.includes('register') ||
-              event.url.includes('admin')
-            ) {
-              this.mostrarBuscador = false;
-            } else {
-              this.mostrarBuscador = true;
-            }
-          }
-        });
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private renderer: Renderer2
+  ) {
+    this.router.events.subscribe((event: RouterEvent) => {
+      if (event instanceof NavigationEnd) {
+        if (
+          event.url.includes('login') ||
+          event.url.includes('register') ||
+          event.url.includes('admin')
+        ) {
+          this.mostrarBuscador = false;
+        } else {
+          this.mostrarBuscador = true;
+        }
+      }
+    });
+
+    this.renderer.listen('window', 'scroll', () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        this.renderer.addClass(this.headerScroll.nativeElement, 'scrolling')
+      } else {
+        this.renderer.removeClass(this.headerScroll.nativeElement, 'scrolling')
+      }
+    })
   }
 
   ngOnInit() {
@@ -36,8 +52,8 @@ export class AppComponent implements OnInit {
     this.authService.esAdmin$.subscribe((data: boolean) => {
       this.admin = data;
     });
-  }
 
+  }
 
 }
 

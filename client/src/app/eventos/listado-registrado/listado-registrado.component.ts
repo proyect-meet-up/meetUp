b36f548e835c51351a51174242fa399931db003f
Borja Arana <barana1977@hotmail.com>
2020-12-17 11:06:50 +0100

    CLIENT: Obtenemos aquellos eventos reservados por el usuario.

--- client/src/app/eventos/listado-registrado/listado-registrado.component.ts
+++ client/src/app/eventos/listado-registrado/listado-registrado.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
+import { EventoService } from '../evento.service';
+import { Usuario } from 'src/app/privado/usuario/usuario.model';
+import { Evento } from '../evento.model';
+import { filter, map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-listado-registrado',
   templateUrl: './listado-registrado.component.html',
-  styleUrls: ['./listado-registrado.component.scss']
+  styleUrls: ['./listado-registrado.component.scss'],
 })
 export class ListadoRegistradoComponent implements OnInit {
+  usuario: Usuario;
+  eventos: Evento[];
 
-  constructor() { }
+  constructor(
+    private eventosService: EventoService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
-  }
+    this.authService.usuario$.subscribe( (data: Usuario) => {
+      this.usuario = data;
+      console.log('Datos del usuario', this.usuario)
 
+    } );
+    this.eventosService.eventosBuscados$
+      .pipe(
+        map((eventos: Evento[]) =>
+          eventos.filter((e) => e.reservas.length !== 0)
+        ),
+        map((eventos) =>
+          eventos.filter((ev) => ev.reservas.find((r) => r === this.usuario._id))
+        ),
+        tap((e) => console.log(e))
+      )
+      .subscribe( (data: Evento[]) => this.eventos = data)
+  }
 }

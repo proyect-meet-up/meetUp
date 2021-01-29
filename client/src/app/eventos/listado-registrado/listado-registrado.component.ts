bb7f3cc314ac095539bd37c0cd95fb51f7038b2f
tritio <tritio2@gmail.com>
2021-01-29 12:25:18 +0100

    CLIENT: corregido error de desuscripción en listado-registrado componente

--- client/src/app/eventos/listado-registrado/listado-registrado.component.ts
+++ client/src/app/eventos/listado-registrado/listado-registrado.component.ts
@@ -1,29 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/auth/auth.service';
 import { EventoService } from '../evento.service';
 import { Usuario } from 'src/app/privado/usuario/usuario.model';
 import { Evento } from '../evento.model';
 import { filter, map, tap } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-listado-registrado',
   templateUrl: './listado-registrado.component.html',
   styleUrls: ['./listado-registrado.component.scss'],
 })
-export class ListadoRegistradoComponent implements OnInit {
+export class ListadoRegistradoComponent implements OnInit, OnDestroy {
   usuario: Usuario;
   eventos: Evento[];
+  suscriptorUsuario: Subscription;
+  suscriptorEventos: Subscription;
 
   constructor(
     private eventosService: EventoService,
     private authService: AuthService
   ) {}
 
-  ngOnInit(): void {
-    this.authService.usuario$.subscribe( (data: Usuario) => {
+  ngOnInit(): void {  
+     
+    this.suscriptorUsuario = this.authService.usuario$.subscribe( (data: Usuario) => {
       this.usuario = data;
     } );
-    this.eventosService.eventosBuscados$
+    this.suscriptorEventos = this.eventosService.eventosBuscados$
       .pipe(
         map((eventos: Evento[]) =>
           eventos.filter((e) => e.reservas.length !== 0)
@@ -33,5 +37,10 @@ export class ListadoRegistradoComponent implements OnInit {
         )
       )
       .subscribe( (data: Evento[]) => this.eventos = data)
+  }
+
+  ngOnDestroy() {
+    this.suscriptorUsuario.unsubscribe();
+    this.suscriptorEventos.unsubscribe();
   }
 }
